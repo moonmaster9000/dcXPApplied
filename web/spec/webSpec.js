13285ce26621d071537572c9caa07ce53fd67565
Matt Parker <pair+mparker@pivotallabs.com>
2017-04-20 16:17:56 -0400

    passing webSpec

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -3,10 +3,42 @@ const ReactDOM = require("react-dom")
 
 
 class PlayForm extends React.Component{
+    constructor(){
+        super()
+
+        this.state = {}
+    }
+
+    buttonHandler(){
+        this.props.useCases.play(this.state.p1Throw, this.state.p2Throw, this)
+    }
+
+    invalid(){
+        this.setState({message: "INVALID"})
+    }
+
+    tie(){
+        this.setState({message: "TIE"})
+    }
+
+    winner(player){
+        this.setState({message: `${player} Wins!`})
+    }
+
+    p1ThrowInputChanged(e){
+        this.setState({p1Throw: e.target.value})
+    }
+
+    p2ThrowInputChanged(e){
+        this.setState({p2Throw: e.target.value})
+    }
+
     render(){
         return <div>
-                INVALID
-                <button type="button"/>
+                {this.state.message}
+                <input type="text" name="p1Throw" onChange={this.p1ThrowInputChanged.bind(this)}></input>
+                <input type="text" name="p2Throw" onChange={this.p2ThrowInputChanged.bind(this)}></input>
+                <button type="button" onClick={this.buttonHandler.bind(this)}/>
             </div>
     }
 }
@@ -22,12 +54,80 @@ describe("play form", function () {
         })
 
         it("tells the user the input was invalid", function () {
+            expect(page()).not.toContain("INVALID")
+
             submitPlayForm()
 
             expect(page()).toContain("INVALID")
         })
-
     })
+
+    describe("play use case reports tie", function () {
+        beforeEach(function () {
+            renderApp({
+                play: function(p1Throw, p2Throw, ui){
+                    ui.tie()
+                }
+            })
+        })
+
+        it("tells the user the input was invalid", function () {
+            expect(page()).not.toContain("TIE")
+
+            submitPlayForm()
+
+            expect(page()).toContain("TIE")
+        })
+    })
+
+    describe("play use case reports p1 winner", function () {
+        beforeEach(function () {
+            renderApp({
+                play: function(p1Throw, p2Throw, ui){
+                    ui.winner("p1")
+                }
+            })
+        })
+
+        it("tells the user the input was invalid", function () {
+            expect(page()).not.toContain("p1 Wins!")
+
+            submitPlayForm()
+
+            expect(page()).toContain("p1 Wins!")
+        })
+    })
+
+    describe("play use case reports p2 winner", function () {
+        beforeEach(function () {
+            renderApp({
+                play: function(p1Throw, p2Throw, ui){
+                    ui.winner("p2")
+                }
+            })
+        })
+
+        it("tells the user the input was invalid", function () {
+            expect(page()).not.toContain("p2 Wins!")
+
+            submitPlayForm()
+
+            expect(page()).toContain("p2 Wins!")
+        })
+    })
+
+    it("sends user input to the play use case", function () {
+        let play = jasmine.createSpy("play")
+
+        renderApp({
+            play: play
+        })
+
+        submitPlayForm("rock", "paper")
+
+        expect(play).toHaveBeenCalledWith("rock", "paper", jasmine.any(Object))
+    })
+
 
     let domFixture
 
@@ -48,8 +148,21 @@ describe("play form", function () {
         setupDOM()
     })
 
-    function submitPlayForm() {
+    afterEach(function () {
+        domFixture.remove()
+    })
+
+    function submitPlayForm(p1Throw, p2Throw) {
+        fillIn("p1Throw", p1Throw)
+        fillIn("p2Throw", p2Throw)
+
         document.querySelector("button").click()
+    }
+
+    function fillIn(inputName, p1Throw) {
+        let element = document.querySelector(`input[name='${inputName}']`)
+        element.value = p1Throw
+        element.dispatchEvent(new Event("input", {bubbles: true, cancelable: false}))
     }
 
     function page() {
